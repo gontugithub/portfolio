4f797b04d4b7406d952efc283f93d28a900f2e00
gontugithub <gs.alvarez8@gmail.com>
2026-01-02 18:57:39 +0100

    feat: router working and basic structure of landing-page and contacts created

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { SimpleRouter } from './router.js';
+import { routes } from './views.js';
+
+new SimpleRouter(routes);
---
+++ src/views.js
@@ -0,0 +1,20 @@
+// Base URL para que funcione en local y en GitHub Pages
+const BASE_URL = import.meta.env.BASE_URL;
+
+export const routes = {
+    '/': {
+        templateId: 'view-landing-page',
+        templateUrl: `${BASE_URL}src/views/landing-page.html`,
+    },
+    '/contacto': {
+        templateId: 'view-contacts',
+        templateUrl: `${BASE_URL}src/views/contacts.html`,
+        onMount: (container) => {
+            console.log("¡Vista de contacto cargada correctamente!");
+        }
+    },
+    404: {
+        templateId: 'view-404',
+        templateUrl: `${BASE_URL}src/views/404.html`
+    },
+};
