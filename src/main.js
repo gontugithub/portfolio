c7157ef1fb5b63aeee342bd358cf0dbe27ea02a0
gontugithub <gs.alvarez8@gmail.com>
2026-01-03 17:47:40 +0100

    feat: hamburger menu for sm display and favicon logo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { SimpleRouter } from './router.js';
 import { routes } from './views.js';
-import { initNavbarScroll } from './navbar.js';
+import { initNavbarScroll, initMobileMenu} from './navbar.js';
 
 new SimpleRouter(routes);
 
 document.addEventListener('DOMContentLoaded', () => {
     initNavbarScroll();
+    initMobileMenu();
     
 });
--- src/navbar.js
+++ src/navbar.js
@@ -20,3 +20,40 @@ export function initNavbarScroll() {
   window.addEventListener('scroll', handleScroll);
   handleScroll();
 }
+
+
+export function initMobileMenu() {
+  const menuBtn = document.getElementById('menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+  
+  if (!menuBtn || !mobileMenu) return;
+
+  const menuIcon = menuBtn.querySelector('i');
+
+  menuBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    mobileMenu.classList.toggle('hidden');
+
+    if (mobileMenu.classList.contains('hidden')) {
+      menuIcon.classList.replace('fa-xmark', 'fa-bars');
+    } else {
+      menuIcon.classList.replace('fa-bars', 'fa-xmark');
+    }
+  });
+
+  // Cerrar al clickar un link
+  mobileMenu.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.add('hidden');
+      menuIcon.classList.replace('fa-xmark', 'fa-bars');
+    });
+  });
+
+  // Cerrar al clickar fuera
+  document.addEventListener('click', (e) => {
+    if (!mobileMenu.classList.contains('hidden') && !menuBtn.contains(e.target) && !mobileMenu.contains(e.target)) {
+      mobileMenu.classList.add('hidden');
+      menuIcon.classList.replace('fa-xmark', 'fa-bars');
+    }
+  });
+}
